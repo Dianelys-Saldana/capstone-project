673041c1c29497c2f6a0380fda35588508dba435
kenneth-cruz1 <kenneth-cruz1>
2022-04-21 12:03:25 -0400

    Added new Server Routes

---
+++ Backend/controller/appointments.py
@@ -0,0 +1,33 @@
+from unittest import result
+from config.dbconfig import pg_config
+import psycopg2
+from model.appointment import AppointmentDAO
+from fastapi.encoders import jsonable_encoder as jsonify
+
+class BaseUsers:
+    def __init__ (self):
+        connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (pg_config["database"], pg_config["username"], pg_config["password"], pg_config["port"], pg_config["host"])
+        self.conn = psycopg2.connect(connection_url)
+
+    def build_row_dict(self, row):
+        result = {
+            "aId": row[0],
+            "userId": row[1],
+            "pId": row[2],
+            "officeId": row[3],
+            "day": row[4],
+            "starttime": row[5],
+            "endtime": row[6],
+        }
+        return result
+    
+    def createAppointment(self, userId, pId, officeId, day, starttime, endtime):
+        dao = AppointmentDAO()
+        aid = dao.createAppointment(userId, pId, officeId, day, starttime, endtime)
+        result = self.build_row_dict((aid, userId, pId, officeId, day, starttime, endtime))
+        return jsonify({
+                'status': "Success",
+                'body': result
+        })
+    
+    
--- Frontend/services/serverroutes.tsx
+++ Frontend/services/serverroutes.tsx
@@ -3,14 +3,13 @@ const url = "http://127.0.0.1:8000";
 export const Server = {
     login,
     signup,
-    /*
     editProfile,
     deleteProfile,
     addProfessional,
     createAppointment,
     viewAppointments,
     addConditions,
-     */
+    manageAppointments,
 };
 
 //User
@@ -30,4 +29,60 @@ function signup(data: any){
     };
     return fetch(url+"/signup", req).then(response => response.json());
 
+}
+function editProfile(data: any){
+    const req = {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/profile", req).then(response => response.json());
+}
+function deleteProfile(data: any){
+    const req = {
+        method: 'delete',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/profile", req).then(response => response.json());
+}
+function addProfessional(data: any){
+    const req = {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/profile", req).then(response => response.json());
+}
+function createAppointment(data: any){
+    const req = {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/appointments", req).then(response => response.json());
+}
+function viewAppointments(data: any){
+    const req = {
+        method: 'get',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/appointments", req).then(response => response.json());
+}
+function addConditions(data: any){
+    const req = {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/profile", req).then(response => response.json());
+}
+function manageAppointments(data: any){
+    const req = {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: data
+    };
+    return fetch(url+"/prof/appointments", req).then(response => response.json());
 }
